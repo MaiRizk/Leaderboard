c6826697aa080b5fb060ef99b8c0a7cb5f1d2e22
Mai Rizk <mai.riizk@gmail.com>
2021-09-11 03:01:04 +0200

    Set static data and work on DOM

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
-/** *** */ (() => { // webpackBootstrap
-/** *** */
+import './style.css';
+import { getScores } from './scores-handler';
 
-/** *** */
-/** *** */
-/** *** */ })();
+document.addEventListener('DOMContentLoaded', () => {
+  getScores();
+});
