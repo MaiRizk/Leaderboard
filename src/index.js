cbf3ff575038e7bf9a1e7c7b4b91bd8c87000035
Mai Rizk <mai.riizk@gmail.com>
2021-09-11 15:20:43 +0200

    fix linter errors

--- src/index.js
+++ src/index.js
@@ -1,68 +1,68 @@
 const apiConfiguration = {
-  baseUrl: "https://us-central1-js-capstone-backend.cloudfunctions.net/api",
-  gameId: "",
+  baseUrl: 'https://us-central1-js-capstone-backend.cloudfunctions.net/api',
+  gameId: '',
+};
+
+const cleanFields = () => {
+  const user = document.querySelector('input[type=name');
+  const score = document.querySelector('input[type=number');
+  user.value = '';
+  score.value = '';
 };
 
 const createGame = async (name) => {
   const response = await fetch(`${apiConfiguration.baseUrl}/games/`, {
-    method: "POST",
+    method: 'POST',
     headers: {
-      "Content-Type": `application/json`,
+      'Content-Type': 'application/json',
     },
     body: JSON.stringify({ name }),
   });
 
   const content = await response.json();
-  const data = content.result.split(" ");
+  const data = content.result.split(' ');
   apiConfiguration.gameId = data[data.length - 2];
   return content;
 };
 
-createGame(`My Amzing game`);
+createGame('My Amzing game');
 
 const addScore = async (data) => {
   const response = await fetch(
     `${apiConfiguration.baseUrl}/games/${apiConfiguration.gameId}/scores/`,
     {
-      method: "POST",
+      method: 'POST',
       headers: {
-        "Content-Type": `application/json`,
+        'Content-Type': 'application/json',
       },
       body: JSON.stringify(data),
-    }
+    },
   );
 
-  const content = await response.json();
+  await response.json();
   cleanFields();
   // handleRefersh();
 };
 
 const handleAddScore = () => {
-  const user = document.querySelector("input[type=name").value;
-  const score = document.querySelector("input[type=number").value;
+  const user = document.querySelector('input[type=name').value;
+  const score = document.querySelector('input[type=number').value;
   addScore({ user, score });
 };
 
-const cleanFields = () => {
-  const user = document.querySelector("input[type=name");
-  const score = document.querySelector("input[type=number");
-  user.value = ``;
-  score.value = ``;
-};
-
 document
-  .getElementById("submit")
-  .addEventListener("click", () => handleAddScore());
+  .getElementById('submit')
+  .addEventListener('click', () => handleAddScore());
 
 const getData = async () => {
   const response = await fetch(
     `${apiConfiguration.baseUrl}/games/${apiConfiguration.gameId}/scores/`,
     {
-      method: "GET",
+      method: 'GET',
       headers: {
-        "Content-Type": `application/json`,
+        'Content-Type': 'application/json',
       },
-    }
+    },
   );
 
   return response.json();
@@ -70,17 +70,18 @@ const getData = async () => {
 
 const handleRefersh = async () => {
   const content = await getData();
-  var table = document.getElementById("scores-table");
+  const table = document.getElementById('scores-table');
   table.innerHTML = '';
   content.result.map((item) => {
-    var row = table.insertRow(0);
-    var cell1 = row.insertCell(0);
-    var cell2 = row.insertCell(1);
+    const row = table.insertRow(0);
+    const cell1 = row.insertCell(0);
+    const cell2 = row.insertCell(1);
     cell1.innerHTML = item.user;
     cell2.innerHTML = item.score;
+    return {};
   });
 };
 
 document
-  .getElementById("refresh")
-  .addEventListener("click", () => handleRefersh());
+  .getElementById('refresh')
+  .addEventListener('click', () => handleRefersh());
